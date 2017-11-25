6bccba386f907b490e1ae199fb72f82d800e6859
Paulo Batista <pauloc@linuxmail.org>
2017-11-25 18:11:27 -0200

    adicionado estrutura do projeto webservice

---
+++ WebServiceCorrente/src/main/java/com/github/costabatista/webservicecorrente/Corrente.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.costabatista.webservicecorrente;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author paulo
+ */
+@Entity
+@Table(name = "CORRENTE")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Corrente.findAll", query = "SELECT c FROM Corrente c")
+    , @NamedQuery(name = "Corrente.findById", query = "SELECT c FROM Corrente c WHERE c.id = :id")
+    , @NamedQuery(name = "Corrente.findByValor", query = "SELECT c FROM Corrente c WHERE c.valor = :valor")
+    , @NamedQuery(name = "Corrente.findByWebservice", query = "SELECT c FROM Corrente c WHERE c.webservice = :webservice")})
+public class Corrente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Long id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "valor")
+    private double valor;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "webservice")
+    private boolean webservice;
+
+    public Corrente() {
+    }
+
+    public Corrente(Long id) {
+        this.id = id;
+    }
+
+    public Corrente(Long id, double valor, boolean webservice) {
+        this.id = id;
+        this.valor = valor;
+        this.webservice = webservice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public boolean getWebservice() {
+        return webservice;
+    }
+
+    public void setWebservice(boolean webservice) {
+        this.webservice = webservice;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Corrente)) {
+            return false;
+        }
+        Corrente other = (Corrente) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.github.costabatista.webservicecorrente.Corrente[ id=" + id + " ]";
+    }
+    
+}
---
+++ WebServiceCorrente/src/main/java/com/github/costabatista/webservicecorrente/service/CorrenteFacadeREST.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.costabatista.webservicecorrente.service;
+
+import com.github.costabatista.webservicecorrente.Corrente;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author paulo
+ */
+@Stateless
+@Path("com.github.costabatista.webservicecorrente.corrente")
+public class CorrenteFacadeREST extends AbstractFacade<Corrente> {
+
+    @PersistenceContext(unitName = "com.github.costabatista_WebServiceCorrente_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    public CorrenteFacadeREST() {
+        super(Corrente.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void create(Corrente entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Long id, Corrente entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Long id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public Corrente find(@PathParam("id") Long id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Corrente> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Corrente> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
